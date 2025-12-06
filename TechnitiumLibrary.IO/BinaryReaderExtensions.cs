ffba37b1658eed78c4fdb5db2b0ab35e27d80bae
Zafer Balkan <zafer@zaferbalkan.com>
2025-12-06 23:19:23 +0200

    Added binary stream size check

--- TechnitiumLibrary.IO/BinaryReaderExtensions.cs
+++ TechnitiumLibrary.IO/BinaryReaderExtensions.cs
@@ -28,7 +28,14 @@ namespace TechnitiumLibrary.IO
     {
         public static byte[] ReadBuffer(this BinaryReader bR)
         {
-            return bR.ReadBytes(ReadLength(bR));
+            int len = ReadLength(bR);
+
+            byte[] buffer = bR.ReadBytes(len);
+
+            if (buffer.Length != len)
+                throw new EndOfStreamException("Unexpected end of stream while reading buffer.");
+
+            return buffer;
         }
 
         public static string ReadShortString(this BinaryReader bR)
@@ -38,32 +45,53 @@ namespace TechnitiumLibrary.IO
 
         public static string ReadShortString(this BinaryReader bR, Encoding encoding)
         {
-            return encoding.GetString(bR.ReadBytes(bR.ReadByte()));
+            int length = bR.ReadByte();
+            byte[] bytes = bR.ReadBytes(length);
+
+            if (bytes.Length != length)
+                throw new EndOfStreamException("Not enough bytes to read short string.");
+
+            return encoding.GetString(bytes);
         }
 
         public static DateTime ReadDateTime(this BinaryReader bR)
         {
-            return DateTime.UnixEpoch.AddMilliseconds(bR.ReadInt64());
+            // Read int64 big-endian timestamp (same as original behavior because .NET native is LE)
+            Span<byte> buffer = stackalloc byte[8];
+            int read = bR.BaseStream.Read(buffer);
+
+            if (read != 8)
+                throw new EndOfStreamException("Not enough bytes to read DateTime ticks.");
+
+            long millis = BinaryPrimitives.ReadInt64LittleEndian(buffer);
+            return DateTime.UnixEpoch.AddMilliseconds(millis);
         }
 
         public static int ReadLength(this BinaryReader bR)
         {
-            int length1 = bR.ReadByte();
-            if (length1 > 127)
-            {
-                int numberLenBytes = length1 & 0x7F;
-                if (numberLenBytes > 4)
-                    throw new IOException("BinaryReaderExtension encoding length not supported.");
+            int first = bR.ReadByte();
+            if (first < 0)
+                throw new EndOfStreamException("Not enough bytes for a length prefix.");
 
-                Span<byte> valueBytes = stackalloc byte[4];
-                bR.BaseStream.ReadExactly(valueBytes.Slice(4 - numberLenBytes, numberLenBytes));
+            // Single byte value
+            if (first <= 127)
+                return first;
 
-                return BinaryPrimitives.ReadInt32BigEndian(valueBytes);
-            }
-            else
-            {
-                return length1;
-            }
+            // Otherwise, multi-byte length
+            int numberLenBytes = first & 0x7F;
+
+            if (numberLenBytes > 4)
+                throw new IOException("BinaryReaderExtension encoding length not supported.");
+
+            Span<byte> temp = stackalloc byte[4];
+
+            int offset = 4 - numberLenBytes;
+            int readBytes = bR.BaseStream.Read(temp[offset..]);
+
+            if (readBytes != numberLenBytes)
+                throw new EndOfStreamException("Not enough bytes for encoded length.");
+
+            return BinaryPrimitives.ReadInt32BigEndian(temp);
         }
     }
 }
