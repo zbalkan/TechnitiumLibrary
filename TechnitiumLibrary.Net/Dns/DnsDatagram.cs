9d88f29172dcceccf58873cdfbf5ba1aa1bef29f
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-11 11:54:44 +0300

    Use fixed-time TSIG MAC comparison
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- TechnitiumLibrary.Net/Dns/DnsDatagram.cs
+++ TechnitiumLibrary.Net/Dns/DnsDatagram.cs
@@ -1434,7 +1434,8 @@ namespace TechnitiumLibrary.Net.Dns
             }
 
             byte[] computedMac = ComputeTsigRequestMac(key.KeyName, key.AlgorithmName, tsig.TimeSigned, tsig.Fudge, tsig.Error, tsig.OtherData, key.SharedSecret, tsig.MAC.Length);
-            if (!computedMac.SequenceEqual(tsig.MAC))
+            // Use fixed-time comparison to avoid timing-dependent MAC verification.
+            if (!CryptographicOperations.FixedTimeEquals(computedMac, tsig.MAC))
             {
                 unsignedRequest = null;
 
@@ -1688,7 +1689,8 @@ namespace TechnitiumLibrary.Net.Dns
             DnsResourceRecord requestTsigRecord = signedRequest._additional[signedRequest._additional.Count - 1];
 
             byte[] computedMac = ComputeTsigResponseMac(requestTsigRecord, key.KeyName, key.AlgorithmName, tsig.TimeSigned, tsig.Fudge, tsig.Error, tsig.OtherData, key.SharedSecret, tsig.MAC.Length);
-            if (!computedMac.SequenceEqual(tsig.MAC))
+            // Use fixed-time comparison to avoid timing-dependent MAC verification.
+            if (!CryptographicOperations.FixedTimeEquals(computedMac, tsig.MAC))
             {
                 unsignedResponse = null;
                 requestFailed = false;
@@ -1814,7 +1816,8 @@ namespace TechnitiumLibrary.Net.Dns
                     }
 
                     byte[] currentComputedMac = ComputeTsigNextResponseMac(priorTsig, dnsMessages, currentTsig.TimeSigned, currentTsig.Fudge, key.SharedSecret, currentTsig.MAC.Length);
-                    if (!currentComputedMac.SequenceEqual(currentTsig.MAC))
+                    // Use fixed-time comparison to avoid timing-dependent MAC verification.
+                    if (!CryptographicOperations.FixedTimeEquals(currentComputedMac, currentTsig.MAC))
                     {
                         unsignedResponse = null;
                         requestFailed = false;
